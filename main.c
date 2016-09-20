33d8ee9b9c25b0796b7f5513151b0b7604d54fd4
user <nobody>
2016-09-20 04:41:34 +0000

    allow names with bad characters since make's $* puts them in there

--- main.c
+++ main.c
@@ -1,10 +1,22 @@
 #include "d2h_convert.h"
 #include <stdlib.h> // exit, getenv
+#include <string.h> // strpbrk
 
 int main(void) {
-	const char* name = getenv("name");
+	char* name = getenv("name");
 	if(name==NULL)
 		exit(1);
+
+	{
+		char* start = name;
+		const char bad[] = "., ?!-";
+		for(;;) {
+			start = strpbrk(start,bad);
+			if(start == NULL) break;
+			*start = '_';
+		}
+	}
+
 
 	d2h_convert(name,1,0);
 }
