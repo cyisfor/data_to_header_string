6384600ece732db8531d93fd6d5fdedf5bb25835
autocommit <autocommit>
2019-05-10 21:17:47 +0000

    auto 0 112 718

--- d2h_convert.h
+++ d2h_convert.h
@@ -1,6 +1,16 @@
 #include <stdbool.h>
 
+/* The ratio of binary / total where we start outputting a {...} style string.  */
+extern float d2h_binary_threshold;
+/* How many characters before forcing a new line (in the source) */
 extern int d2h_max_width;
+/* Do we define a macro, or define a const unsigned char[] variable? */
 extern bool d2h_define_macro;
+/* Is our variable static? */
 extern bool d2h_static_vars;
+
+/*
+	convert source to a C array of bytes saved to dest, with the name "name"
+	"name_length" is also defined, the length of that array.
+*/
 void d2h_convert(const char* name, int dest, int source);
