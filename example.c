b6e31587101edb40951c6774da063764ddaa2151
autocommit <autocommit>
2019-05-10 17:14:58 +0000

    auto 0 33 348

---
+++ example.c
@@ -0,0 +1,13 @@
+#include "first_example.h"
+#include "example2.h"
+int main(int argc, char *argv[]) {
+#ifdef EXAMPLE_ONE_YAY
+  puts("Yay, we defined a macro.");
+  fwrite(EXAMPLE_ONE_YAY, EXAMPLE_ONE_YAY_length, 1, stdout);
+  fputc('\n', stdout);
+#endif
+
+  puts("Here's the second example:");
+  fwrite(second_example, second_example_length, 1, stdout);
+	return 0;
+}
