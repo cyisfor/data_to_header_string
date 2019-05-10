6705fb82d33e5b0220a044711f5a3b73da44d90f
autocommit <autocommit>
2019-05-10 19:57:59 +0000

    auto 0 16 213

--- example.c
+++ example.c
@@ -1,11 +1,13 @@
 #include "first_example.h"
-#include "example2.h"
 int main(int argc, char *argv[]) {
 #ifdef EXAMPLE_ONE_YAY
   puts("Yay, we defined a macro.");
   fwrite(EXAMPLE_ONE_YAY, EXAMPLE_ONE_YAY_length, 1, stdout);
   fputc('\n', stdout);
 #endif
+
+	/* INSIDE a function! */
+#include "example2.h"
 
   puts("Here's the second example:");
   fwrite(second_example, second_example_length, 1, stdout);
